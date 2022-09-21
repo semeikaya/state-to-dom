0d122d436b48bb92f5c2777aeb2d264dc3997e65
Bersyak <semeikayaskaevih@gmail.com>
2022-09-21 08:52:32 +0300

    Share rep

--- state-dom.js
+++ state-dom.js
@@ -1,28 +1,42 @@
 const doing = [
-    { text: "первое" },
-    { text: "второе" },
-    { text: "третье" },
-    { text: "четвертое" },
-    { text: "пятое" },
+    { text: "первое", done: true },
+    { text: "второе", done: true },
+    { text: "третье", done: true },
+    { text: "четвертое", done: true },
+    { text: "пятое", done: true },
 ];
 
+const remove = (i) => {
+    doing.splice(i - 1, 1);
+    render(doing);
+};
+const addTodo = (text) => {
+    doing.push({ text: text, done: true });
+    render(doing);
+};
+
+const btn = document.querySelector("#btn");
+const inpt = document.querySelector("#input");
+
 const render = (arr) => {
-    
     const div = document.querySelector("#list");
-    div.innerHTML = '';
+    div.innerHTML = "";
     for (const ar of arr) {
-        const li = document.createElement('div')
-        li.style.backgroundColor = 'red'
-        li.style.marginBottom = '15px'
-        li.textContent = ar.text
-        div.append(li)
+        const li = document.createElement("div");
+        const del = document.createElement("input");
+        li.className = "mark";
+        del.className = "del";
+        li.textContent = ar.text;
+        del.type = "button";
+        div.append(li);
+        li.append(del);
+        del.addEventListener("click", () => {
+
+        });
     }
 };
-const remove = (i) => {
-    doing.splice(i-1, 1)
-    render(doing)
-}
-render(doing)
-remove(4)
 
-
+btn.addEventListener("click", () => {
+    addTodo(inpt.value);
+    inpt.value = "";
+});
